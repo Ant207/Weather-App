9f2016122b2f9115e98d109f26a8ecc12bc5bee4
Ant207 <207.Tran@gmail.com>
2026-07-08 16:46:45 -0400

    code complete

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import {renderWeather} from './modules/domController.js';
 const searchBtn = document.getElementById('search-button');
 
 searchBtn.addEventListener('click', () => {
+    const container = document.getElementById('container');
     const location = document.getElementById('location-input').value;
-
+    container.textContent = "Loading...";
     fetchWeather(location)
         .then(summary => {
             renderWeather(summary);
         })
-        .catch(error => console.log("error occurred:", error));
+        .catch(error => container.textContent = "Please enter a valid location.");
 });
